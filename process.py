db15f538832676b358be9f478cf09fb54cf50ac9
Cho KyoungJae <88143806+bestKUFO@users.noreply.github.com>
2024-10-01 23:01:32 +0900

    주소 데이터 추가 및 주소 반환 API 추가

--- process.py
+++ process.py
@@ -3,8 +3,13 @@ import requests
 
 app = Flask(__name__)
 
+# 전역 변수로 주소 저장
+address_box = None
+
 @app.route('/flaskapi/<int:id>', methods=['POST'])
 def send_to_gpu_server(id):
+    global address_box  # 전역 변수 사용
+
     if 'image' not in request.files:
         return jsonify({"에러": "이미지 없음"}), 400
 
@@ -24,14 +29,22 @@ def send_to_gpu_server(id):
         if ocr_response.ok:
             ocr_data = ocr_response.json()
             call_menu_item = []  # 메뉴 아이템 초기화
-
+            
             # OCR 데이터에서 필요한 정보를 추출
             if '메뉴 및 가격' in ocr_data:
                 call_menu_item = ocr_data['메뉴 및 가격']
-            
-            
+                
+            # 주소 정보를 저장
+            address_box = ocr_data.get('주소', None)
+            # 주소에서 후처리
+            if address_box:
+                address_box = address_box.replace(' 1중', '').replace(' 1층', '').strip()
+
             # 추가적인 처리 로직
-            return jsonify({"메뉴 및 가격": call_menu_item}), 200
+            return jsonify({
+                "메뉴 및 가격": call_menu_item,
+                "주소": address_box  # 주소도 반환
+            }), 200
             
         else:
             return jsonify({"OCR 요청 실패": ocr_response.text}), 500
@@ -39,6 +52,14 @@ def send_to_gpu_server(id):
     except Exception as e:
         return jsonify({"이미지 전송 오류": str(e)}), 500
 
+# 이미지에서 추출한 주소 정보 반환
+@app.route('/get_address', methods=['GET'])
+def get_address():
+    global address_box
+    if address_box:
+        return jsonify({"주소": address_box}), 200
+    else:
+        return jsonify({"에러": "주소 정보가 없습니다."}), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
